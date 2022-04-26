4225b3d301d4929f2883e17333e84f8f00c384dc
Tim La <tlamarketing111@gmail.com>
2022-04-26 10:50:16 -0400

    added semicolons

--- ecommerce/sanity_ecommerce/schemas/schema.js
+++ ecommerce/sanity_ecommerce/schemas/schema.js
@@ -1,5 +1,5 @@
-import createSchema from 'part:@sanity/base/schema-creator'
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import createSchema from 'part:@sanity/base/schema-creator';
+import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 import product from './product';
 import banner from './banner';
